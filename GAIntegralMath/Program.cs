9903f9d7b599fd7c2200afc9c4bf814b9bb6c998
Adam Ishak <iadam9862@gmail.com>
2024-01-17 20:08:11 -0800

    Add project files.

---
+++ GAIntegralMath/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GAIntegralMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
